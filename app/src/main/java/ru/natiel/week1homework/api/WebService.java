735331224db9f919c7c54ad1ebd418379f8fff5d
NatielSanti <natielsanti@mail.ru>
2020-05-10 17:39:01 +0300

    Merge remote-tracking branch 'origin/third_week' into third_week
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/ru/natiel/week1homework/BudgetFragment.java
    #	app/src/main/java/ru/natiel/week1homework/web/Api.java
    #	app/src/main/java/ru/natiel/week1homework/web/WebService.java

--- app/src/main/java/ru/natiel/week1homework/api/WebService.java
+++ app/src/main/java/ru/natiel/week1homework/api/WebService.java
@@ -5,6 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import ru.natiel.week1homework.BuildConfig;
 
 public class WebService {
     private static volatile WebService instance;
